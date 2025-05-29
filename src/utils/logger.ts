f0d4aeec31baae6401d88926ac71f74ebb5a352f
fredagbona <fredagbona@gmail.com>
2025-05-29 15:50:22 +0100

    chore: add simple structured logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,1 +1,5 @@
-// lint-staged check
+export const log = {
+  info: (...args: unknown[]) => console.log('[INFO]', ...args),
+  warn: (...args: unknown[]) => console.warn('[WARN]', ...args),
+  error: (...args: unknown[]) => console.error('[ERROR]', ...args),
+};
